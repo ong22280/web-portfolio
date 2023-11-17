2c1b7a3b3c7f7615a7aa13aa1b1418f742e98a2e
Sittipong Hemloun <ong22280@gmail.com>
2023-11-18 00:59:11 +0700

    redesign(about)

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -62,41 +62,36 @@ export default function AboutPage() {
         <div className="flex justify-center flex-shrink-0 mb-6 md:justify-start">
           <Image
             src={profile}
-            width={300}
+            width={250}
             alt="NextUI hero Image"
             className="border-8 rounded-full shadow-lg border-primary "
           />
         </div>
         {/* content */}
-        <aside className="ml-12 text-left">
-          <p>
-            After graduating with a degree in{" "}
-            <span className="font-medium">Accounting</span>, I decided to pursue
-            my passion for programming. I enrolled in a coding bootcamp and
-            learned{" "}
-            <span className="font-medium">full-stack web development</span>.{" "}
-            <span className="italic">My favorite part of programming</span> is
-            the problem-solving aspect. I{" "}
-            <span className="underline">love</span> the feeling of finally
-            figuring out a solution to a problem. My core stack is{" "}
-            <span className="font-medium">
-              React, Next.js, Node.js, and MongoDB
-            </span>
-            . I am also familiar with TypeScript and Prisma. I am always looking
-            to learn new technologies. I am currently looking for a{" "}
-            <span className="font-medium">full-time position</span> as a
-            software developer.
+        <div className="ml-12 text-left">
+          <h1 className="mb-4 text-xl font-bold">
+            Hello, I&apos;m{" "}
+            <span className="text-primary">Sittipong Hemluan</span> (Ong).
+          </h1>
+
+          <p className="mb-4 ">
+            Currently a 3rd-year{" "}
+            <span className="font-semibold ">Computer Science</span> student at{" "}
+            <span className="font-semibold ">Kasetsart University</span>.
+          </p>
+
+          <p className="mb-4 ">
+            Passionate about{" "}
+            <span className="font-semibold">web development</span>, I love
+            building things and experimenting. Always on the lookout for new
+            opportunities to improve my coding skills and more.
           </p>
 
           <p>
-            <span className="italic">When I&apos;m not coding</span>, I enjoy
-            playing video games, watching movies, and playing with my dog. I
-            also enjoy <span className="font-medium">learning new things</span>.
-            I am currently learning about{" "}
-            <span className="font-medium">history and philosophy</span>.
-            I&apos;m also learning how to play the guitar.
+            I sincerely hope to be a part of some of the coolest innovations in
+            the future.
           </p>
-        </aside>
+        </div>
       </main>
 
       <Divider className="container px-6 mx-auto max-w-7xl" />
